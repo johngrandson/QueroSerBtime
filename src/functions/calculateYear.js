63ca8befa0f1fb29e3332ff804a863d2f4e445d4
João Netto <joaonettopb@hotmail.com>
2019-03-11 13:05:10 -0300

    comments added

--- src/functions/calculateYear.js
+++ src/functions/calculateYear.js
@@ -1,7 +1,9 @@
 module.exports = async (data) => {
+        // data returned inside of consume method
         if (data % 4 === 0) {
                 if (data % 100 !== 0) return true
                 else return false
         } else if(data % 400 === 0) return true
         else return false
+        // will return true or false based on it's value
 }
--- src/services/api.js
+++ src/services/api.js
@@ -6,9 +6,12 @@ const url = "http://numbersapi.com/random/year";
 
 const consume = util.promisify(request);
 
+// consume the API data.
 consume(url).then(data => {
+    //  then extract the first number in the string
     data = data.body.match(/\d+/)[0];
 
+    // returns true or false as value
     return calculateYears(data);
 
 }).catch(err => console.log('error: ', err))
